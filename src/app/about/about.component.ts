3c531c533e9f57705ce6fea8cef421f920789806
Mary Prouty <meprouty@gatech.edu>
2019-09-05 21:57:18 -0400

    Framed photo

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IImage } from 'ng-simple-slideshow/src/app/modules/slideshow/IImage';
 
 @Component({
   selector: 'app-about',
@@ -7,14 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  public imagesUrl;
+  public imagesUrl: IImage[];
 
   constructor() { }
 
   ngOnInit() {
     this.imagesUrl = [
-      '../assets/coffee.jpg', '../assets/pounce.png',
-      '../assets/running.jpg'
+      {url: '../assets/coffee.jpg', caption: 'Modeling attack tactics with a cup a joe'},
+      {url: '../assets/pounce.png', caption: 'Level one of my Gameboy game Pounce!'},
+      {url: '../assets/running.jpg', caption: 'Racing for GT at the Roy Griak Invitational'}
     ];
   }
 
